490e0f3bd455c5fb41f0d9a738221c8701eb0c4c
pavel-256 <paveldev320@gmail.com>
2023-06-17 14:44:45 +0300

    Update rest_app.py

--- files/backend_testing.py
+++ files/backend_testing.py
@@ -2,15 +2,15 @@ import json
 
 import requests
 
-# with requests.post('http://127.0.0.1:5000/users/1', json={"user_name": "John"}) as res1:
-#     if res1.ok:
-#         print(res1.json())
-#         with requests.get('http://127.0.0.1:5000/users/1') as res2:
-#             result = json.loads(res2.text)
-#             if res2.status_code == 200 and result['user name'] == 'John':
-#                 print("The user_name is John and status 200")
-#                 if result['user id'] == "1":
-#                     print('User John is under id 1')
+with requests.post('http://127.0.0.1:5000/users/1', json={"user_name": "John"}) as res1:
+    if res1.ok:
+        print(res1.json())
+        with requests.get('http://127.0.0.1:5000/users/1') as res2:
+            result = json.loads(res2.text)
+            if res2.status_code == 200 and result['user name'] == 'John':
+                print("The user_name is John and status 200")
+                if result['user id'] == "1":
+                    print('User John is under id 1')
 
 
 
