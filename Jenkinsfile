6cb3e2eb6213d175da19f78f47a541ea48534a60
pavel-256 <paveldev320@gmail.com>
2023-06-24 12:49:47 +0300

    Update Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,26 +4,32 @@ pipeline {
     stages {
         stage('Pull code from GitHub') {
             steps {
-                // Pull code from your Github repository
-                git branch: 'main', url: 'https://github.com/pavel-256/DevOpsProject.git'
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: 'main']],
+                    userRemoteConfigs: [[url: 'https://github.com/pavel-256/DevOpsProject.git']]
+                ])
             }
         }
 
         stage('Install modules') {
             steps {
-                bat 'py -m pip install flask'
-                bat 'py -m pip install pymysql'
-                bat 'py -m pip install selenium'
-                bat 'py -m pip install requests'
+                script {
+                    bat '''
+                    py -m pip install flask
+                    py -m pip install pymysql
+                    py -m pip install selenium
+                    py -m pip install requests
+                    '''
+                }
             }
         }
 
         stage('Run rest_app.py') {
             steps {
                 script {
-                    // Start rest_app.py as a background process
                     if (isUnix()) {
-                        sh 'nohup python rest_app.py &'
+                        sh 'nohup python files/rest_app.py &'
                     } else {
                         bat 'start /B py files/rest_app.py'
                     }
@@ -33,7 +39,7 @@ pipeline {
 
         stage('Run backend_testing.py') {
             steps {
-                bat 'py files/docker_backend_testing.py'
+                bat 'py files/backend_testing.py'
             }
         }
 
@@ -46,17 +52,16 @@ pipeline {
         stage('Push Docker image') {
             steps {
                 script {
-                    // Read the Docker Hub credentials from the .env file
-                    def dockerHubUsername = readFile('.env').readLines().find { it.startsWith('DOCKER_USERNAME=') }?.substring('DOCKER_USERNAME='.length())
-                    def dockerHubPassword = readFile('.env').readLines().find { it.startsWith('DOCKER_PASSWORD=') }?.substring('DOCKER_PASSWORD='.length())
+                    dir('files') {
+                        def envFile = readFile('.env')
+                        def dockerHubUsername = envFile.readLines().find { it.startsWith('DOCKER_USERNAME=') }?.substring('DOCKER_USERNAME='.length())
+                        def dockerHubPassword = envFile.readLines().find { it.startsWith('DOCKER_PASSWORD=') }?.substring('DOCKER_PASSWORD='.length())
+                        def imageName = envFile.readLines().find { it.startsWith('IMAGE_NAME=') }?.substring('IMAGE_NAME='.length())
 
-                    // Read the image name from the .env file
-                    def imageName = readFile('.env').readLines().find { it.startsWith('IMAGE_NAME=') }?.substring('IMAGE_NAME='.length())
-
-                    // Login to Docker Hub and push the image
-                    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-                        bat "docker login -u ${dockerHubUsername} -p ${dockerHubPassword}"
-                        bat "docker push ${dockerHubUsername}/${imageName}"
+                        withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                            sh "docker login -u ${dockerHubUsername} -p ${dockerHubPassword}"
+                            sh "docker push ${dockerHubUsername}/${imageName}"
+                        }
                     }
                 }
             }
@@ -64,7 +69,7 @@ pipeline {
 
         stage('Set compose image version') {
             steps {
-                sh 'echo "IMAGE_TAG=${BUILD_NUMBER}" > .env'
+                sh 'echo "IMAGE_TAG=${BUILD_NUMBER}" > files/.env'
             }
         }
 
@@ -83,8 +88,7 @@ pipeline {
         stage('Clean environment') {
             steps {
                 script {
-                    // Read the image name from the .env file
-                    def imageName = readFile('.env').readLines().find { it.startsWith('IMAGE_NAME=') }?.substring('IMAGE_NAME='.length())
+                    def imageName = readFile('files/.env').readLines().find { it.startsWith('IMAGE_NAME=') }?.substring('IMAGE_NAME='.length())
                     bat "docker-compose down"
                     bat "docker rmi ${imageName}"
                 }
--- files/rest_app.py
+++ files/rest_app.py
@@ -1,127 +1,65 @@
-import pymysql
-import requests
+import signal
 from flask import Flask, request
-# request
-# from db_connector import get_request, post_request, delete_request, put_request
+
+from db_connector import get_request, post_request, delete_request, put_request
 import os
 from datetime import date
 
-app = Flask(__name__)
-
-schema_name = "DevOps"
-dataBase = 'Devops.users'
-dbColumns = 'user_name,user_id,creation_date'
 # local users storage
 users = {}
 
-# Establishing a connection to the database
-
-host = os.environ.get('DB_HOST', 'localhost')
-user = os.environ.get('DB_USER', 'root')
-password = os.environ.get('DB_PASSWORD', '')
-database = os.environ.get('DB_NAME', '')
-
-# Establish connection to the database
-conn = pymysql.connect(host=host, user=user, passwd=password, db=database)
-cursor = conn.cursor()
-conn.autocommit(True)
+app = Flask(__name__)
 
 
-@app.route('/users/<user_id>', methods=['GET', 'POST'])
-def get_users(user_id):
+# supported methods
+@app.route('/users/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def user(user_id):
     if request.method == 'GET':
-        sql = f"SELECT * FROM {dataBase} WHERE  user_id = %s"
+        try:
+            # Get username from request
+            result = get_request(user_id)
+            return {'user id': user_id, 'user name': result[0][1], 'status': 'ok'}, 200
 
-        cursor.execute(sql, user_id)
+        except:
+            return {'reason': 'no such id', 'status': 'error'}, 400
 
-        # Close the connection
-        cursor.close()
-
-        # Return the response
-        result = cursor.fetchall()
-        return result
     elif request.method == 'POST':
 
-        # getting the json data payload from request
-        request_data = request.json
+        try:
+            # Get username from request
+            user_name = post_request(user_id)
+            return {'user id': user_id, 'user_added': user_name, 'status': 'ok'}, 200  # status code
 
-        # treating request_data as a dictionary to get a specific value from key
-        user_name = request_data.get('user_name')
-        users[user_id] = user_name
+        except:
+            return {'status': "error", 'reason': 'id already exist'}, 500  # status code
 
-        # Define the query with placeholders for the variables
-        sql = f"INSERT INTO {dataBase}  ({dbColumns}) VALUES(%s,%s,%s)"
+    elif request.method == 'DELETE':
 
-        # Execute the query with the values
-        values = (user_name, user_id, date.today())
+        try:
+            # Get username form request
+            user_name = delete_request(user_id)
+            return {'user id': user_id, 'user_deleted': user_name, 'status': 'ok'}, 200  # status code
 
-        # Execute the query with the values
-        cursor.execute(sql, values)
+        except:
+            return {'status': "error", 'reason': 'no such id'}, 500  # status code
 
-        return {'user id': user_id, 'user_added': user_name, 'status': 'ok'}, 200  # status code
+    elif request.method == 'PUT':
+
+        try:
+            user_name = put_request(user_id)
+            return {'user_updated': user_name, 'status': 'ok'}, 200  # status code
+
+        except:
+            return {'status': "error", 'reason': 'no such user id'}, 500  # status code
 
 
+@app.route('/stop_server')
+def stop_server():
+    try:
+        os.kill(os.getpid(), signal.CTRL_C_EVENT)
+        return 'Server stopped'
+    except:
+        return 'Server failed to stop'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
 
-# local users storage
-# users = {}
-#
-# app = Flask(__name__)
-#
-#
-# # supported methods
-# @app.route('/users/<user_id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
-# def user(user_id):
-#     if request.method == 'GET':
-#         try:
-#             # Get username from request
-#             result = get_request(user_id)
-#             return {'user id': user_id, 'user name': result[0][1], 'status': 'ok'}, 200
-#
-#         except:
-#             return {'reason': 'no such id', 'status': 'error'}, 400
-#
-#     elif request.method == 'POST':
-#
-#         try:
-#             # Get username from request
-#             user_name = post_request(user_id)
-#             return {'user id': user_id, 'user_added': user_name, 'status': 'ok'}, 200  # status code
-#
-#         except:
-#             return {'status': "error", 'reason': 'id already exist'}, 500  # status code
-#
-#     elif request.method == 'DELETE':
-#
-#         try:
-#             # Get username form request
-#             user_name = delete_request(user_id)
-#             return {'user id': user_id, 'user_deleted': user_name, 'status': 'ok'}, 200  # status code
-#
-#         except:
-#             return {'status': "error", 'reason': 'no such id'}, 500  # status code
-#
-#     elif request.method == 'PUT':
-#
-#         try:
-#             user_name = put_request(user_id)
-#             return {'user_updated': user_name, 'status': 'ok'}, 200  # status code
-#
-#         except:
-#             return {'status': "error", 'reason': 'no such user id'}, 500  # status code
-#
-#
-# @app.route('/stop_server')
-# def stop_server():
-#     try:
-#         os.kill(os.getpid(), signal.CTRL_C_EVENT)
-#         return 'Server stopped'
-#     except:
-#         return 'Server failed to stop'
-#
-#
-# # todo elif for put and delete
-#
-# app.run(host='127.0.0.1', debug=True, port=5000)
+app.run(host='127.0.0.1', debug=True, port=5000)
