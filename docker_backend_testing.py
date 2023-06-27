8d5c197ddca26f0beed316e139467bc27acb3355
pavel-256 <paveldev320@gmail.com>
2023-06-27 16:26:33 +0300

    update  docker_backend_testing.py

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -1,5 +1,15 @@
 import requests
 
-# Example test code
-response = requests.get('http://127.0.0.1:5000/users/1')
-print(response.json())
+# Define the base URL of the Dockerized application
+base_url = "http://localhost:5000"
+
+# Perform a GET request to the API endpoint
+response = requests.get(f"{base_url}/api/endpoint")
+
+# Check the response status code
+if response.status_code == 200:
+    print("Dockerized app is running successfully!")
+    print("Response:", response.text)
+else:
+    print("Error: Failed to access the Dockerized app.")
+    print("Response status code:", response.status_code)
