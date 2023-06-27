265485cab7f9d55584d17b9887f6cf76d1a630a1
pavel-256 <paveldev320@gmail.com>
2023-06-27 17:42:04 +0300

    update

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -1,15 +1,16 @@
 import requests
 
-# Define the base URL of your Dockerized app
-base_url = "http://localhost:8080"
-
-# Make a GET request to test the endpoint
-response = requests.get(f"{base_url}")
-
-# Check the response status code and content
-if response.status_code == 200:
-    print("Dockerized app is running successfully!")
-    print("Response:", response.text)
-else:
-    print("Error: Dockerized app is not running properly.")
-    print("Response:", response.text)
+print('ok')
+# # Define the base URL of your Dockerized app
+# base_url = "http://localhost:8080"
+#
+# # Make a GET request to test the endpoint
+# response = requests.get(f"{base_url}")
+#
+# # Check the response status code and content
+# if response.status_code == 200:
+#     print("Dockerized app is running successfully!")
+#     print("Response:", response.text)
+# else:
+#     print("Error: Dockerized app is not running properly.")
+#     print("Response:", response.text)
