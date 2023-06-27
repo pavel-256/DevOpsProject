b148543834fce1ea02f66994c0294e9875746f4a
pavel-256 <paveldev320@gmail.com>
2023-06-27 17:26:15 +0300

    update

--- docker_backend_testing.py
+++ docker_backend_testing.py
@@ -1,7 +1,7 @@
 import requests
 
 # Define the base URL of your Dockerized app
-base_url = "http://localhost:5000"
+base_url = "http://localhost:8080"
 
 # Make a GET request to test the endpoint
 response = requests.get(f"{base_url}/api/test")
